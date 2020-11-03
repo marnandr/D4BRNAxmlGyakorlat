cc968084f007049651aba971a0c2aae0a4a2a38c
marnandr <maronandor97@gmail.com>
2020-11-03 15:20:35 +0100

    DomParser under construction

---
+++ D4BRNA_1103/domd4brna/DomD4BRNA.java
@@ -0,0 +1,54 @@
+package D4BRNA_1103.domd4brna;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
+
+public class DomD4BRNA {
+    public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
+        File xmlFile = new File("D4BRNA_1103/szemelyek.xml");
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = factory.newDocumentBuilder();
+        Document doc = dBuilder.parse(xmlFile);
+
+        doc.getDocumentElement().normalize();
+
+        System.out.println("Gyökér elem: " + doc.getDocumentElement().getNodeName());
+
+        NodeList nList = doc.getElementsByTagName("szemely");
+
+        for (int i = 0; i < nList.getLength(); i++) {
+            Node nNode = nList.item(i);
+
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element elem = (Element) nNode;
+
+                String uid = elem.getAttribute("id");
+
+                Node node1 = elem.getElementsByTagName("nev").item(0);
+                String nev = node1.getTextContent();
+
+                Node node2 = elem.getElementsByTagName("kor").item(0);
+                String kor = node2.getTextContent();
+
+                Node node3 = elem.getElementsByTagName("nev").item(0);
+                String varos = node3.getTextContent();
+
+                System.out.println("id: " + uid);
+                System.out.println("\tnév: " + nev);
+                System.out.println("\tkor: " + kor);
+                System.out.println("\tváros: " + varos);
+
+            }
+        }
+    }
+}
---
+++ D4BRNA_1103/domd4brna/DomParserD4BRNA.java
@@ -0,0 +1,59 @@
+package D4BRNA_1103.domd4brna;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
+public class DomParserD4BRNA {
+    public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
+        File xmlFile = new File("D4BRNA_1103/student.xml");
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = factory.newDocumentBuilder();
+        Document doc = dBuilder.parse(xmlFile);
+
+        doc.getDocumentElement().normalize();
+
+        System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+        NodeList nList = doc.getElementsByTagName("class");
+        System.out.println("--------------------------------------------------------");
+
+        for (int i = 0; i < nList.getLength(); i++) {
+            Node nNode = nList.item(i);
+
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element elem = (Element) nNode;
+
+                
+
+                String rollno = elem.getAttribute("rollno");
+
+                Node node1 = elem.getElementsByTagName("firstname").item(0);
+                String firstname = node1.getTextContent();
+
+                Node node2 = elem.getElementsByTagName("lastname").item(0);
+                String lastname = node2.getTextContent();
+
+                Node node3 = elem.getElementsByTagName("nickname").item(0);
+                String nickname = node3.getTextContent();
+
+                Node node4 = elem.getElementsByTagName("age").item(0);
+                String age = node4.getTextContent();
+
+                System.out.println("rollno: " + rollno);
+                System.out.println("név: " + firstname);
+                System.out.println("kor: " + lastname);
+                System.out.println("város: " + nickname);
+                System.out.println("age: " + age);
+
+            }
+        }
+    }
+}
